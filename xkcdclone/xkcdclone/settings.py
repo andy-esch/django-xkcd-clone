3fde18b2cb47acc2774f73b138304349a206e009
Andy Eschbacher <andyesch@protonmail.com>
2018-05-26 21:02:34 -0400

    adds xkcd to installed apps to abs register template

--- xkcdclone/xkcdclone/settings.py
+++ xkcdclone/xkcdclone/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'xkcd.apps.XkcdConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'xkcdclone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['xkcdclone/xkcd/templates', ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
